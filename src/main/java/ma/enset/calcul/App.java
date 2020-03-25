c37332945d1e2637a94aaecf640cb0c5be3c4240
myfirstOne <meryem.elmchichi@gmail.com>
2020-03-25 13:42:04 +0100

    My first comit

---
+++ src/main/java/ma/enset/calcul/App.java
@@ -0,0 +1,13 @@
+package ma.enset.calcul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
